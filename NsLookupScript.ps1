9d3fb951ae3ed4f933118be4ca15bb1679831426
lourdhuraj <153839340+lourdhuraj@users.noreply.github.com>
2023-12-14 20:08:35 +0530

    Create NsLookupScript.ps1

---
+++ NsLookupScript.ps1
@@ -0,0 +1,27 @@
+# Code by Lourdhu Raj
+# Function to perform nslookup
+function Get-NsLookupResults {
+    param (
+        [string]$domain
+    )
+
+    try {
+        $result = Resolve-DnsName -Name $domain -ErrorAction Stop
+        $result
+    } catch {
+        Write-Host "Error looking up $($domain): $_"
+    }
+}
+
+# Read input file with domain names
+$inputFile = "C:\Path\To\Input\File.txt"
+$domains = Get-Content $inputFile
+
+# Output file
+$outputFile = "C:\Path\To\Output\Output.txt"
+
+# Loop through each domain, perform nslookup, and store the output in the text file
+foreach ($domain in $domains) {
+    $nslookupResult = Get-NsLookupResults -domain $domain
+    $nslookupResult | Out-File -Append -FilePath $outputFile
+}
